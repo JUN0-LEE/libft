9cb7d2580f9a47f85f4e115aa33f9417e8a03843
Junho Lee <julee@e1z2r3p23.42.us.org>
2020-01-30 14:31:04 -0800

    makefile

---
+++ src/another_tool/ft_swap.c
@@ -0,0 +1,13 @@
+
+#include "../../includes/libft.h"
+
+void    ft_swap(void *a, void *b, size_t size) {
+
+    void    *tmp;
+
+    tmp = ft_memalloc(size);
+    ft_memmove(tmp, a, size);
+    ft_memmove(a, b, size);
+    ft_memmove(b, tmp, size);
+    ft_memdel(&tmp);
+}
